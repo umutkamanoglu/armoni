939965319e17332bec94b7c93baf885273d1c36e
umutkamanoglu <kamanogluu@gmail.com>
2026-01-21 22:34:59 +0300

    Header da ufak düzenleme yapıldı.

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,11 +1,12 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
 export default function AppHeader() {
   return (
     <SafeAreaView edges={['top']} className="">
-      <View className="justify-cente h-14 items-center">
-        <Text className="text-lg font-bold">Custom Header</Text>
+      <View className="flex h-14 flex-row items-center justify-between px-[5vw]">
+        <Text className="text-2xl font-bold">Armoni</Text>
+        <Ionicons name="settings-outline" size={24} color="black" />
       </View>
     </SafeAreaView>
   );
